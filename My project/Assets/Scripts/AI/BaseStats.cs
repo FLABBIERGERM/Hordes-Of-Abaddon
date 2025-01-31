02c63647d4d1d27910061886282db6a7ba0445e0
FLABBIERGERM <96157270+FLABBIERGERM@users.noreply.github.com>
2025-01-31 13:27:49 -0700

    Added the hud scoring
    currently fixed the hud got that all working but now working on the round system which is bugged.

--- My project/Assets/Scripts/AI/BaseStats.cs
+++ My project/Assets/Scripts/AI/BaseStats.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BaseStats : MonoBehaviour, IDamageAble
 {
+    public static BaseStats Instance;
+
+
     SpawnManager spawnManager = new SpawnManager();
     public float zDamage;
     public float zHealth;
-   // public float zMaxHealth;
-    GameManager gameManager;
+    public UnityEvent enemyKilled;
+
+    // public float zMaxHealth;
     public bool IsAlive => zHealth > 0;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
     //float currentZHP;
     private void Start()
     {
@@ -22,11 +34,12 @@ public class BaseStats : MonoBehaviour, IDamageAble
         zHealth -= damage;
         Debug.Log("Remaing Zombie HP" + zHealth);
         if (zHealth <= 0)
-        {
-            gameManager.essence += 25f;
+        {        
             spawnManager.EnemyKill();
             Debug.Log("Okay the zombie has died"); // go b ack through all the code and remember where the zombie dying is
+            enemyKilled.Invoke();
             Destroy(gameObject);
+            
         }
     }
 }
--- My project/Assets/Scripts/Core/SpawnManager.cs
+++ My project/Assets/Scripts/Core/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -17,59 +18,74 @@ public class SpawnManager : MonoBehaviour
 
     private Queue<GameObject> enemiesToSpawn = new Queue<GameObject>();
 
-    private int enemiesRemaining = 0;
+    private int enemiesRemaining = 0 ;
 
-
+    private int totalEnemies = 0;
 
     private void Awake()
     {
         Instance = this;
     }
 
-
+    public void FixedUpdate()
+    {
+        //CheckOnEnemies();
+    }
     public void StartSpawning(int round)
     {
-        int numOfEnemies = round *  2; // change number later based on difficulty
+        int numOfEnemies = round *  3; // change number later based on difficulty
         Debug.Log(" So we are doing StartSpawning and the num of enemies there should be is :" + numOfEnemies);
         for (int i = 0; i < numOfEnemies; i++) 
         { 
             enemiesToSpawn.Enqueue(enemyMonster);
         }
+        totalEnemies = enemiesToSpawn.Count;
         StartCoroutine(SpawningEnemies());
     }
-
     private IEnumerator SpawningEnemies()
     {
         Debug.Log("We have started the spawning enemies Coroutine");
         while(enemiesToSpawn.Count > 0)
         {
-            Debug.Log("We are in the while loop of the enemies to spawn coroutine.");
+            //Debug.Log("We are in the while loop of the enemies to spawn coroutine.");
             GameObject enemy = Instantiate(
 
                 enemiesToSpawn.Dequeue(),
                 spawnPoints[Random.Range(0,spawnPoints.Length)].position,
                 Quaternion.identity);
-
-
             AiStateController stateController = enemy.GetComponent<AiStateController>();
             if (stateController != null && stateController.aiBlackboard != null)
             {
                 stateController.aiBlackboard.chaseTarget = playerToFollow;
             }
+            BaseStats enemyStats = enemy.GetComponent<BaseStats>();
+            if(enemyStats != null && HudScore.Instance != null)
+            {
+                HudScore.Instance.RegisterEnemy(enemyStats);
+            }
             animatorM.SetBool("Spawned", false);
             enemiesRemaining++;
-            yield return new WaitForSeconds(8f);
+            Debug.Log("Enemys remaining" + enemiesRemaining);
+            yield return new WaitForSeconds(3f);
         }
-        RoundManager.Instance.currentRoundState = RoundManager.RoundState.RoundPlaying;
-    }
+        if(enemiesRemaining <= 0 || totalEnemies <=0)
+        {
+            Debug.Log("enemies died to fast good job!");
 
+            RoundManager.Instance.EnemyDefeated();
+        }
+
+    }
     public void EnemyKill()
     {
+        totalEnemies--;
         enemiesRemaining--;
-
-        if(enemiesRemaining <= 0 && RoundManager.Instance.currentRoundState == RoundManager.RoundState.RoundPlaying)
+        Debug.Log("New enemies remaining" + enemiesRemaining);
+       // Debug.Log("Total Enemies" + totalEnemies);
+        if (enemiesRemaining <= 0 && RoundManager.Instance.currentRoundState == RoundManager.RoundState.RoundPlaying)
         {
             RoundManager.Instance.EnemyDefeated();
         }
     }
+
 }
