0f03db6a8af67dc1e574046033a5fa3e4005d2f1
FLABBIERGERM <96157270+FLABBIERGERM@users.noreply.github.com>
2025-03-11 17:48:46 -0600

    BAse Particle Effects
    Add on hit effects and shots fired effects for the rifle and a more stable jump action for the attacks currently gonna work on that more as its no implemented yet.

--- My project/Assets/Scripts/AI/FiniteStateMachine/Actions/Jump_Attack.cs
+++ My project/Assets/Scripts/AI/FiniteStateMachine/Actions/Jump_Attack.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 
 public class Jump_Attack : Action
 {
-    private void PerformJAttack(AIBlackBoard aiblackBoard)
-    {
-        Vector3 PDistance = Vector3.Distance((aiBlackboard.navMeshAgent.transform.position, aiBlackboard.chaseTarget.position));
 
+    public override void Act(Blackboard blackboard)
+    {
+        if (blackboard is AIBlackBoard aiBlackboard)
+        {
+            PerformJAttack(aiBlackboard);
+        }
+    }
+    private void PerformJAttack(AIBlackBoard aiBlackboard)
+    {
+        float PDistance = Vector3.Distance(aiBlackboard.navMeshAgent.transform.position, aiBlackboard.chaseTarget.position);
+        Debug.Log("The distance Between the Enemy Jumping and The Player is: " +  PDistance);
         if(PDistance >= 20)
         {
             // This is where the lerp will go i think. and i also think we dont need the if statment as that would be a conditional thing i think.
