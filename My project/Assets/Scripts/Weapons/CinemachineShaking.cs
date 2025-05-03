f1fb7a99084e6aeaac8f472178250d159151072b
FLABBIERGERM <96157270+FLABBIERGERM@users.noreply.github.com>
2025-05-02 18:24:27 -0600

    Weapon Texture and Gun Physicallity
    added in the texture for the gun to make it feel better and also added int he weapon physicallity like camera shake.

---
+++ My project/Assets/Scripts/Weapons/CinemachineShaking.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CinemachineShaking : MonoBehaviour
+{
+    public static CinemachineShaking Instance { get; private set; }
+
+    private CinemachineCamera cinemachineCamera;
+    private float shakeTimer;
+    private float startingIntesnsity;
+    private float shakeTimerTotal;
+
+    [SerializeField] CinemachineImpulseSource CISource;
+    [SerializeField] float powerAmount;
+
+    private void Awake()
+    {
+        Instance = this;
+        cinemachineCamera = GetComponent<CinemachineCamera>();
+
+    }
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin CinemachineNoise = cinemachineCamera.GetComponent<CinemachineBasicMultiChannelPerlin>();
+        CinemachineNoise.AmplitudeGain = intensity;
+
+        startingIntesnsity = intensity;
+        shakeTimerTotal = time; 
+        shakeTimer = time;
+    }
+    public void ScreenShake(Vector3 dir) // this is the easiest way to do this rather than my other code.
+    {
+        CISource.GenerateImpulseWithVelocity(dir);
+    }
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                CinemachineBasicMultiChannelPerlin CinemachineNoise = cinemachineCamera.GetComponent<CinemachineBasicMultiChannelPerlin>();
+                CinemachineNoise.AmplitudeGain = 0f;
+
+                Mathf.Lerp(startingIntesnsity, 0f, 1-(shakeTimer / shakeTimerTotal));
+            }
+        }
+    }
+}
