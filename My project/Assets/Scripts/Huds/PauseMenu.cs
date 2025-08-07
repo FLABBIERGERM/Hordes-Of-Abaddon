1ca3df1f4418ed53bd682021007b7aab96d49b48
FLABBIERGERM <96157270+FLABBIERGERM@users.noreply.github.com>
2025-08-07 11:35:00 -0700

    Please dont delete this
    for the love of all that is holy dont you dare kill this

--- My project/Assets/Scripts/Huds/PauseMenu.cs
+++ My project/Assets/Scripts/Huds/PauseMenu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
+using System;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,21 +12,53 @@ public class PauseMenu : MonoBehaviour
     private VisualElement root;
     private Button resumeButton;
     private Button quitButton;
-    private Button fontChange;
-    [SerializeField] private Font gothic;
-    [SerializeField] private Font basic;
-    private bool fonton = true;
+    private Slider sensSliderX;
+    private Slider sensSliderY;
+    [SerializeField] private CinemachineInputAxisController cameraz;
+    //private Button fontChange;
+    //[SerializeField] private Font gothic;
+    //[SerializeField] private Font basic;
+    //private bool fonton = true;
     void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
 
         resumeButton = root.Q<Button>("resume-button");
         quitButton = root.Q<Button>("quit-button");
-        fontChange = root.Q<Button>("Font_Change");
-        fontChange.clicked += fontChangeClicked;
+        // fontChange = root.Q<Button>("Font_Change");
+        //fontChange.clicked += fontChangeClicked;
+        sensSliderX = root.Q<Slider>("SensSliderX");
+        sensSliderY = root.Q<Slider>("SensSliderY");
+
         resumeButton.clicked += ResumeButtonPressed;
         quitButton.clicked += QuitButtonPressed;
+        sensSliderX.RegisterValueChangedCallback(evt =>
+        {
+            float newSens = evt.newValue;
+            Debug.Log("The slider is changing so that works" + newSens);
+            //var axisController = GetComponent<CinemachineInputAxisController>();
+            foreach (var c in cameraz.Controllers)
+            {
+                if (c.Name == "Look X (Pan)")
+                {
+                    c.Input.Gain = Mathf.Round(newSens*100f)/100f;
 
+                }
+            }
+        });
+        sensSliderY.RegisterValueChangedCallback(evt =>
+        {
+            float newSens = evt.newValue;
+            Debug.Log("The slider is changing so that works" + newSens);
+            //var axisController = GetComponent<CinemachineInputAxisController>();
+            foreach (var c in cameraz.Controllers)
+            {
+                if (c.Name == "Look Y (Tilt)")
+                {
+                    c.Input.Gain = Mathf.Round(newSens * 100f) / 100f;
+                }
+            }
+        });
         GameState.Instance.OnGamePaused.AddListener(ReceivedOnGamePaused);
         GameState.Instance.OnGameResumed.AddListener(ReceivedOnGameResumed);
 
@@ -35,7 +69,7 @@ public class PauseMenu : MonoBehaviour
     {
         resumeButton.clicked -= ResumeButtonPressed;
         quitButton.clicked -= QuitButtonPressed;
-        fontChange.clicked -= fontChangeClicked;
+        //fontChange.clicked -= fontChangeClicked;
         GameState.Instance.OnGamePaused.RemoveListener(ReceivedOnGamePaused);
         GameState.Instance.OnGameResumed.RemoveListener(ReceivedOnGameResumed);
     }
@@ -45,25 +79,25 @@ public class PauseMenu : MonoBehaviour
         GameManager.Instance.ResumeGame();
 
     }
-    private void fontChangeClicked()
-    {
-        fonton = !fonton;
-        Font selectedFont = fonton ? gothic : basic;
-        root.style.unityFont = selectedFont;
-        fontSetter(resumeButton, selectedFont);
-        fontSetter(quitButton, selectedFont);
-        fontSetter(fontChange, selectedFont);
+    //private void fontChangeClicked()
+    //{
+    //    fonton = !fonton;
+    //    Font selectedFont = fonton ? gothic : basic;
+    //    root.style.unityFont = selectedFont;
+    //    fontSetter(resumeButton, selectedFont);
+    //    fontSetter(quitButton, selectedFont);
+    //    fontSetter(fontChange, selectedFont);
 
-    }
-    private void fontSetter(Button button, Font font)
-    {
-        var label = button.Q<Label>();
-        if (label != null)
-        {
-            label.style.unityFont = font;
-        }
+    //}
+    //private void fontSetter(Button button, Font font)
+    //{
+    //    var label = button.Q<Label>();
+    //    if (label != null)
+    //    {
+    //        label.style.unityFont = font;
+    //    }
 
-    }
+    //}
     private void QuitButtonPressed()
     {
         SceneManager.LoadScene("MainMenu");
